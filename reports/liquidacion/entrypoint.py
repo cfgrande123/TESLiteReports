c930d1fddc311f59c6368b6b63aa46cd3fb7da44
cfgrande123 <99143283+cfgrande123@users.noreply.github.com>
2024-09-20 15:39:01 +0200

    Cambio req por assets

--- reports/liquidacion/entrypoint.py
+++ reports/liquidacion/entrypoint.py
@@ -6,19 +6,18 @@
 
 from connect.client import R
 
-from ..utils import convert_to_datetime, get_basic_value, get_value, today_str
+from ..utils import convert_to_datetime, get_value
 
 HEADERS = (
-    'Request ID', 'Request Type', 'Request Status',
-    'Created At', 'Updated At', 'Exported At',
+    'Subscription ID', 'Subscription External ID', 'Vendor primary key',
+    'Subscription Type', 'Creation date', 'Updated date', 'Status', 'Billing Period',
+    'Anniversary Day', 'Anniversary Month', 'Contract ID', 'Contract Name',
     'Customer ID', 'Customer Name', 'Customer External ID',
     'Tier 1 ID', 'Tier 1 Name', 'Tier 1 External ID',
     'Tier 2 ID', 'Tier 2 Name', 'Tier 2 External ID',
-    'Marketplace ID', 'Marketplace Name',
-    'Provider  ID', 'Provider Name', 'Vendor ID', 'Vendor Name',
-    'Product ID', 'Product Name',
-    'Asset ID', 'Asset External ID', 'Transaction Type',
-    'Hub ID', 'Hub Name', 'Asset Status',
+    'Provider Account ID', 'Provider Account name',
+    'Vendor Account ID', 'Vendor Account Name',
+    'Product ID', 'Product Name', 'Hub ID', 'Hub Name',
 )
 
 
@@ -29,93 +28,130 @@ def generate(
     renderer_type=None,
     extra_context_callback=None,
 ):
-    requests = _get_requests(client, parameters)
+    products_primary_keys = {}
+    subscriptions = _get_subscriptions(client, parameters)
+    total = subscriptions.count()
     progress = 0
-    total = requests.count()
     if renderer_type == 'csv':
         yield HEADERS
         progress += 1
         total += 1
         progress_callback(progress, total)
 
-    for request in requests:
-        connection = request['asset']['connection']
+    for subscription in subscriptions:
+        primary_vendor_key = get_primary_key(
+            subscription.get('params', []),
+            subscription['product']['id'],
+            client,
+            products_primary_keys,
+        )
         if renderer_type == 'json':
             yield {
                 HEADERS[idx].replace(' ', '_').lower(): value
-                for idx, value in enumerate(_process_line(request, connection))
+                for idx, value in enumerate(_process_line(subscription, primary_vendor_key))
             }
         else:
-            yield _process_line(request, connection)
+            yield _process_line(subscription, primary_vendor_key)
         progress += 1
         progress_callback(progress, total)
 
 
-def _get_requests(client, parameters):
-    all_status = ['tiers_setup', 'inquiring', 'pending', 'approved', 'failed']
-
+def _get_subscriptions(client, parameters):
     query = R()
-    query &= R().updated.ge(parameters['date']['after'])
-    query &= R().updated.le(parameters['date']['before'])
-    query &= R().rr_type.eq("Purchase")
-    query &= R().rr_status.eq("Approved")
-    
+    if parameters.get('date') and parameters['date']['after'] != '':
+        query &= R().events.created.at.ge(parameters['date']['after'])
+        query &= R().events.created.at.le(parameters['date']['before'])
+    if parameters.get('product') and parameters['product']['all'] is False:
+        query &= R().product.id.oneof(parameters['product']['choices'])
+    if parameters.get('mkp') and parameters['mkp']['all'] is False:
+        query &= R().marketplace.id.oneof(parameters['mkp']['choices'])
+    if parameters.get('period') and parameters['period']['all'] is False:
+        query &= R().billing.period.uom.oneof(parameters['period']['choices'])
+    if parameters.get('status') and parameters['status']['all'] is False:
+        query &= R().status.oneof(parameters['status']['choices'])
 
-   # if parameters.get('product') and parameters['product']['all'] is False:
-   #     query &= R().asset.product.id.oneof(parameters['product']['choices'])
-   # if parameters.get('rr_type') and parameters['rr_type']['all'] is False:
-   #     query &= R().type.oneof(parameters['rr_type']['choices'])
-   # if parameters.get('rr_status') and parameters['rr_status']['all'] is False:
-   #     query &= R().status.oneof(parameters['rr_status']['choices'])
-   # else:
-   #     query &= R().status.oneof(all_status)
-   # if parameters.get('mkp') and parameters['mkp']['all'] is False:
-   #     query &= R().asset.marketplace.id.oneof(parameters['mkp']['choices'])
-   # if parameters.get('hub') and parameters['hub']['all'] is False:
-   #     query &= R().asset.connection.hub.id.oneof(parameters['hub']['choices'])
-
-    return client.requests.filter(query).select(
-        '-asset.items',
-        '-asset.params',
-        '-asset.configuration',
-        '-activation_key',
-        '-template',
-    )
+    return client.ns('subscriptions').assets.filter(query)
 
 
-def _process_line(request, connection):
+def calculate_period(delta, uom):
+    if delta == 1:
+        if uom == 'monthly':
+            return 'Monthly'
+        return 'Yearly'
+    else:
+        if uom == 'monthly':
+            return f'{int(delta)} Months'
+        return f'{int(delta)} Years'
+
+
+def get_anniversary_day(subscription_billing):
+    if 'anniversary' in subscription_billing and 'day' in subscription_billing['anniversary']:
+        return subscription_billing['anniversary']['day']
+    return '-'
+
+
+def get_anniversary_month(subscription_billing):
+    if 'anniversary' in subscription_billing and 'month' in subscription_billing['anniversary']:
+        return subscription_billing['anniversary']['month']
+    return '-'
+
+
+def search_product_primary(parameters):
+    for param in parameters:
+        if param['constraints'].get('reconciliation'):
+            return param['name']
+
+
+def get_primary_key(parameters, product_id, client, products_primary_keys):
+    try:
+        if product_id not in products_primary_keys:
+            prod_parameters = client.collection(
+                'products',
+            )[product_id].collection(
+                'parameters',
+            ).all()
+            primary_id = search_product_primary(prod_parameters)
+            products_primary_keys[product_id] = primary_id
+        for param in parameters:
+            if param['id'] == products_primary_keys[product_id]:
+                return param['value'] if 'value' in param and len(param['value']) > 0 else '-'
+    except ClientError:
+        pass
+    return '-'
+
+
+def _process_line(subscription, primary_vendor_key):
     return (
-        get_basic_value(request, 'id'),
-        get_basic_value(request, 'type'),
-        get_basic_value(request, 'status'),
-        convert_to_datetime(
-            get_basic_value(request, 'created'),
-        ),
-        convert_to_datetime(
-            get_basic_value(request, 'updated'),
-        ),
-        today_str(),
-        get_value(request['asset']['tiers'], 'customer', 'id'),
-        get_value(request['asset']['tiers'], 'customer', 'name'),
-        get_value(request['asset']['tiers'], 'customer', 'external_id'),
-        get_value(request['asset']['tiers'], 'tier1', 'id'),
-        get_value(request['asset']['tiers'], 'tier1', 'name'),
-        get_value(request['asset']['tiers'], 'tier1', 'external_id'),
-        get_value(request['asset']['tiers'], 'tier2', 'id'),
-        get_value(request['asset']['tiers'], 'tier2', 'name'),
-        get_value(request['asset']['tiers'], 'tier2', 'external_id'),
-        get_value(request['asset'], 'marketplace', 'id'),
-        get_value(request['asset'], 'marketplace', 'name'),
-        get_value(request['asset']['connection'], 'provider', 'id'),
-        get_value(request['asset']['connection'], 'provider', 'name'),
-        get_value(request['asset']['connection'], 'vendor', 'id'),
-        get_value(request['asset']['connection'], 'vendor', 'name'),
-        get_value(request['asset'], 'product', 'id'),
-        get_value(request['asset'], 'product', 'name'),
-        get_value(request, 'asset', 'id'),
-        get_value(request, 'asset', 'external_id'),
-        get_value(request['asset'], 'connection', 'type'),
-        get_value(connection, 'hub', 'id') if 'hub' in connection else '',
-        get_value(connection, 'hub', 'name') if 'hub' in connection else '',
-        get_value(request, 'asset', 'status'),
+        subscription.get('id'),
+        subscription.get('external_id', '-'),
+        primary_vendor_key,
+        get_value(subscription, 'connection', 'type'),
+        convert_to_datetime(subscription['events']['created']['at']),
+        convert_to_datetime(subscription['events']['updated']['at']),
+        subscription.get('status'),
+        calculate_period(
+            subscription['billing']['period']['delta'],
+            subscription['billing']['period']['uom'],
+        ) if 'billing' in subscription else '-',
+        get_anniversary_day(subscription['billing']) if 'billing' in subscription else '-',
+        get_anniversary_month(subscription['billing']) if 'billing' in subscription else '-',
+        subscription['contract']['id'] if 'contract' in subscription else '-',
+        subscription['contract']['name'] if 'contract' in subscription else '-',
+        get_value(subscription.get('tiers', ''), 'customer', 'id'),
+        get_value(subscription.get('tiers', ''), 'customer', 'name'),
+        get_value(subscription.get('tiers', ''), 'customer', 'external_id'),
+        get_value(subscription.get('tiers', ''), 'tier1', 'id'),
+        get_value(subscription.get('tiers', ''), 'tier1', 'name'),
+        get_value(subscription.get('tiers', ''), 'tier1', 'external_id'),
+        get_value(subscription.get('tiers', ''), 'tier2', 'id'),
+        get_value(subscription.get('tiers', ''), 'tier2', 'name'),
+        get_value(subscription.get('tiers', ''), 'tier2', 'external_id'),
+        get_value(subscription['connection'], 'provider', 'id'),
+        get_value(subscription['connection'], 'provider', 'name'),
+        get_value(subscription['connection'], 'vendor', 'id'),
+        get_value(subscription['connection'], 'vendor', 'name'),
+        get_value(subscription, 'product', 'id'),
+        get_value(subscription, 'product', 'name'),
+        get_value(subscription['connection'], 'hub', 'id'),
+        get_value(subscription['connection'], 'hub', 'name'),
     )
